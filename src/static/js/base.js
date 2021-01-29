2c97cb4d840689ab234ce4b7ca7c5b94e4ec0898
Sanojan Thiyagaraja <sanojan99@gmail.com>
2021-01-30 08:59:00 +1100

    maintain scroll position when clicking in/out of forms in course view

--- src/static/js/base.js
+++ src/static/js/base.js
@@ -1,15 +1,35 @@
-// const container = document.querySelector('#container');
-console.log(3)
 
-let btn = document.querySelector('#btn');
-let divList = document.querySelector('.rando'); 
+// let btn = document.querySelector('#btn');
+// let divList = document.querySelector('.rando'); 
 
-btn.addEventListener('click', () => {
-    if(divList.style.display =='none') {
-        divList.style.display = 'block';
-    } else  {
-        divList.style.display = 'none';
+// btn.addEventListener('click', () => {
+//     if(divList.style.display =='none') {
+//         divList.style.display = 'block';
+//     } else  {
+//         divList.style.display = 'none';
+//     }
+// })
+
+
+let container = document.querySelector('main')
+
+
+var startingScrollPosition = localStorage.getItem("scrollPosition") || 0;
+// console.log(startingScrollPosition);
+container.scrollTop = startingScrollPosition;
+
+
+
+var links = document.querySelectorAll("a");
+console.log(link)
+
+for (var i = 0; i < links.length; i++) {
+    link = links[i];
+    link.onclick = function () {
+        // console.log(container.scrollTop);
+        let scrollPosition = parseInt(container.scrollTop);
+        localStorage.setItem("scrollPosition", scrollPosition);
+        //console.log(container.scrollTop)
     }
 
-})
-
+}
